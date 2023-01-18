7b56321b955c24e0983993ae8130ec0c4e00c61f
zhangchao1 <zhangchao1@neucloud.cn>
2023-01-18 10:20:17 +0800

    初始化项目

---
+++ src/extension.js
@@ -0,0 +1,19 @@
+const vscode = require('vscode');
+
+/**
+ * 插件被激活时触发，所有代码总入口
+ * @param {*} context 插件上下文
+ */
+exports.activate = function(context) {
+    console.log('恭喜，您的扩展“vscode-plugin-demo”已被激活！');
+    require('./helloword')(context); // helloworld
+    require('./webview')(context); // Webview
+    require('./welcome')(context); // 欢迎提示
+};
+
+/**
+ * 插件被释放时触发
+ */
+exports.deactivate = function() {
+    console.log('您的扩展“vscode-plugin-demo”已被释放！')
+};
